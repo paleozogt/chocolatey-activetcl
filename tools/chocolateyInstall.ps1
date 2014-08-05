fac5dccd94d719bbb983e3c1f8899085f1a7babc
Aaron Simmons <asimmons@sensoryinc.com>
2014-08-05 15:11:11 -0600

    ActiveTcl package pulled from ActiveTcl.8.5.14.002

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$Name='ActiveTcl'
+$Url32='http://downloads.activestate.com/ActiveTcl/releases/8.5.14.0/ActiveTcl8.5.14.0.296777-win32-ix86-threaded.exe'
+$Url64='http://downloads.activestate.com/ActiveTcl/releases/8.5.14.0/ActiveTcl8.5.14.0.296777-win32-x86_64-threaded.exe'
+
+Install-ChocolateyPackage $name exe '' $url32 $url64
