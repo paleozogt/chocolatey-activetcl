f92f5c06515a4e73af70b8d6f3ff0285e0611dd3
Aaron Simmons <paleozogt@gmail.com>
2014-08-05 15:56:02 -0600

    providing the install dir to the installer so that it will be (mostly) silent

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,4 +2,4 @@
 $Url32='http://downloads.activestate.com/ActiveTcl/releases/8.6.1.0/ActiveTcl8.6.1.0.297577-win32-ix86-threaded.exe'
 $Url64='http://downloads.activestate.com/ActiveTcl/releases/8.6.1.0/ActiveTcl8.6.1.0.297577-win32-x86_64-threaded.exe'
 
-Install-ChocolateyPackage $name exe '' $url32 $url64
+Install-ChocolateyPackage $name exe '--directory C:\Tcl' $url32 $url64
