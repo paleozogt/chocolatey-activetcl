94d4c873d7d775f68709bd0f9ba950c913f9994b
Aaron Simmons <paleozogt@gmail.com>
2014-08-05 15:29:07 -0600

    upgrading to ActiveTcl 8.6.1.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$Name='ActiveTcl'
-$Url32='http://downloads.activestate.com/ActiveTcl/releases/8.5.14.0/ActiveTcl8.5.14.0.296777-win32-ix86-threaded.exe'
-$Url64='http://downloads.activestate.com/ActiveTcl/releases/8.5.14.0/ActiveTcl8.5.14.0.296777-win32-x86_64-threaded.exe'
+$Url32='http://downloads.activestate.com/ActiveTcl/releases/8.6.1.0/ActiveTcl8.6.1.0.297577-win32-ix86-threaded.exe'
+$Url64='http://downloads.activestate.com/ActiveTcl/releases/8.6.1.0/ActiveTcl8.6.1.0.297577-win32-x86_64-threaded.exe'
 
 Install-ChocolateyPackage $name exe '' $url32 $url64
