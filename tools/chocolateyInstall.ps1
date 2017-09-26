a97acdf990a517c7d3b93e668b021f640b1a4b51
Aaron Simmons <paleozogt@gmail.com>
2017-09-26 20:23:32 +0100

    ActiveTcl 8.6.1.0 link is dead. Updating to 8.6.6.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$Name='ActiveTcl'
-$Url32='http://downloads.activestate.com/ActiveTcl/releases/8.6.1.0/ActiveTcl8.6.1.0.297577-win32-ix86-threaded.exe'
-$Url64='http://downloads.activestate.com/ActiveTcl/releases/8.6.1.0/ActiveTcl8.6.1.0.297577-win32-x86_64-threaded.exe'
+$Url32=''
+$Url64='http://downloads.activestate.com/ActiveTcl/releases/8.6.6.8606/ActiveTcl-8.6.6.8606-MSWin32-x64-401995.exe'
 
-Install-ChocolateyPackage $name exe '--directory C:\Tcl' $url32 $url64
+Install-ChocolateyPackage $name exe /qn $url32 $url64
