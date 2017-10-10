d15e3743de88b5fe27dd791544d85ee6c80a80f8
Aaron Simmons <paleozogt@gmail.com>
2017-10-10 10:55:58 -0600

    32-bit tcl

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$Name='ActiveTcl'
-$Url32=''
+$Url32='http://downloads.activestate.com/ActiveTcl/releases/8.6.6.8607/ActiveTcl-8.6.6.8607-MSWin32-x86-403667.exe'
 $Url64='http://downloads.activestate.com/ActiveTcl/releases/8.6.6.8606/ActiveTcl-8.6.6.8606-MSWin32-x64-401995.exe'
 
 Install-ChocolateyPackage $name exe /qn $url32 $url64
